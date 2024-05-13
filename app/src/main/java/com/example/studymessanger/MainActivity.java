c725fe4ed2dc2e2828f04dcacddfd8abc35186c8
Иван Кузнецов <superfly85@mail.ru>
2024-05-13 23:57:53 +0500

    11.3. add auth user

--- app/src/main/java/com/example/studymessanger/MainActivity.java
+++ app/src/main/java/com/example/studymessanger/MainActivity.java
@@ -1,19 +1,76 @@
 package com.example.studymessanger;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+
+        auth.sendPasswordResetEmail("email@email,ru").addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d(TAG, "Success");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "NOT Success");
+            }
+        });
+//        FirebaseUser user = auth.getCurrentUser();
+//        auth.signInWithEmailAndPassword("email123@email.ru", "123456")
+//                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+//                    @Override
+//                    public void onSuccess(AuthResult authResult) {
+//                        Log.d(TAG, "User auth ");
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.d(TAG, "User not Auth!");
+//                    }
+//                });
+//        auth.createUserWithEmailAndPassword("email123@email.ru", "123456")
+//                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+//                    @Override
+//                    public void onSuccess(AuthResult authResult) {
+//                        if (user == null) {
+//                            Log.d(TAG, "not auth user");
+//                        } else {
+//                            Log.d(TAG, "correct user");
+//                        }
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.d(TAG, e.getMessage());
+//                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//        auth.signOut();
     }
 }
